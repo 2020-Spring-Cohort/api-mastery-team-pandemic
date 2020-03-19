b86380e6bf52804d371918804ef5e159cdfe28f9
Noah Doughty <noahdoughty@gmail.com>
2020-03-18 20:02:39 -0400

    Created all constructors for Team Pojo

--- backend-api/src/main/java/org/wcci/apimastery/models/Team.java
+++ backend-api/src/main/java/org/wcci/apimastery/models/Team.java
@@ -17,6 +17,34 @@ public class Team {
     public Team() {
     }
 
+    public Team(Long id, String name, Conference conference, Stats stats) {
+        this.conference = conference;
+        this.name = name;
+        this.stats = stats;
+    }
+
+    public Team(String name, Stats stats) {
+        this.name = name;
+        this.stats = stats;
+    }
+
+    public Team(String name, Conference conference) {
+        this.name = name;
+        this.conference = conference;
+    }
+
+    public Team(String name) {
+        this.name = name;
+    }
+
+    public Team(Stats stats) {
+        this.stats = stats;
+    }
+
+    public Team(Conference conference) {
+        this.conference = conference;
+    }
+
     public String getName() {
         return name;
     }
